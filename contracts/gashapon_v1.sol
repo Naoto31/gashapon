890c5b606e9c40c6fd20296ff002355dbc2c2851
Naoto31 <naoto0910sai@gmail.com>
2022-11-05 21:48:53 +0100

    wip

--- contracts/gashapon_v1.sol
+++ contracts/gashapon_v1.sol
@@ -5,3 +5,26 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Gashapon_V1 is ERC721URIStorage {
+
+    address payable owner;
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    Counters.Counter private _itemsSold;
+
+    uint256 listPrice = 0.001 ether; // this is needed when users list their own NFT to market place
+
+    constructor() ERC721("Gashapon_V1", "GASHA") {
+        owner = payable(msg.sender);
+    }
+
+    struct ListedToken {
+        uint256 tokenId;
+        address payable owner;
+        address payable seller;
+        uint256 price;
+        bool currentlyListed;   
+    }
+}
