9c207838503ed1f4e42d8786045e771093f79581
Naoto31 <naoto0910sai@gmail.com>
2022-11-06 03:55:22 +0100

    comment out

--- pages/components/modals/oneNft.tsx
+++ pages/components/modals/oneNft.tsx
@@ -10,12 +10,12 @@ const OneNftComponent = (props: {
   console.log("here")
   return (
     <Modal open={props.show} noPadding>
-      <Modal.Header css={{position: "absolute", zIndex: "$1", top: 5, right: 8}}>
+      {/* <Modal.Header css={{position: "absolute", zIndex: "$1", top: 5, right: 8}}>
         <Text color='#363449'>test</Text>
       </Modal.Header>
       <Modal.Body>
         <Image showSkeleton src={props.data.image} width={400} height={490} />
-      </Modal.Body>
+      </Modal.Body> */}
     </Modal>
   )
 }
